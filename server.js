6ba2f9084c701bf8959d33f62fb9695000b74093
wsimmalavong <wsimmalavong@ryerson.ca>
2021-06-06 15:11:13 -0400

    fix

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/stratuslit'));
 
-app.get('/*', (req, res) =>
+app.get('/', (req, res) =>
     res.sendFile('index.html', {root: 'dist/stratuslit/'}),
 );
 
 app.get('/weather', (request, response)=>{
-    res.json({"Hello" : "Hello"})
+    res.send({"Hello" : "Hello"})
 });
 
 // Start the app by listening on the default Heroku port
